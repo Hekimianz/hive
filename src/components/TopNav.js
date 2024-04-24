4b978a81624b6529c94ee2063434460a26e4a550
Hekimianz <aramhekdesign@gmail.com>
2024-04-24 11:54:59 -0600

    added assets

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,6 +1,11 @@
 import styles from "./css/TopNav.module.css";
+import logo from "../assets/hiveLogo.png";
 function TopNav() {
-  return <div className={styles.topNavCont}>This Is The Top Nav</div>;
+  return (
+    <div className={styles.topNavCont}>
+      <img className={styles.logo} src={logo} />
+    </div>
+  );
 }
 
 export default TopNav;
